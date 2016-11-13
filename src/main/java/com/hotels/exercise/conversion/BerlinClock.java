faae8e22a4c6bae9f282f0f790dc7c98e1b9ec76
Javier Garcia <jgarciacot@wiley.com>
2016-11-13 13:43:37 +0000

    Refactored test

--- src/main/java/com/hotels/exercise/conversion/BerlinClock.java
+++ src/main/java/com/hotels/exercise/conversion/BerlinClock.java
@@ -2,8 +2,12 @@ package com.hotels.exercise.conversion;
 
 import java.time.DateTimeException;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.StringJoiner;
 
 public class BerlinClock {
+
+    private static final String TIME24HOURS_PATTERN = "([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
 
     public String convert(int hour, int min, int sec) {
         if (isValidTime(hour, min, sec)) {
@@ -12,14 +16,38 @@ public class BerlinClock {
         return "Unable to convert digital time to Berlin Clock representation";
     }
 
+    /**
+     * Validates the unit times are valid
+     * @param hour Hour
+     * @param min Minutes
+     * @param sec Seconds
+     * @return true valid time format, false invalid time format
+     */
     private boolean isValidTime(int hour, int min, int sec)  {
         try {
-            final LocalTime localTime = LocalTime.of(hour, min, sec);
-            return true;
+
+            String time = convertToTextTime(hour, min, sec);
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(TIME24HOURS_PATTERN);
+            return LocalTime.parse(time, dateTimeFormatter) != null;
         }
         catch (DateTimeException dateTimeException) {
             dateTimeException.printStackTrace();
         }
         return false;
+    }
+
+    /**
+     * Returns the text representation of an hour, minutes and seconds
+     * @param hour Hour
+     * @param min Minutes
+     * @param sec Seconds
+     * @return Text representation in format HH:mm:ss
+     */
+    private String convertToTextTime(int hour, int min, int sec) {
+        StringJoiner time = new StringJoiner(":");
+        time.add(String.valueOf(hour));
+        time.add(String.valueOf(min));
+        time.add(String.valueOf(sec));
+        return time.toString();
     }
 }
--- src/test/java/com/hotels/exercise/conversion/BerlinClockTest.java
+++ src/test/java/com/hotels/exercise/conversion/BerlinClockTest.java
@@ -1,17 +1,24 @@
 package com.hotels.exercise.conversion;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(JUnitParamsRunner.class)
 public class BerlinClockTest {
 
     @Test
-    public void shouldConvert24hTime() {
+    @Parameters({"13, 60, 00", "24, 00, 00", "0, 0, 0"})
+    public void shouldConvert24hTime(int hour, int min, int sec) {
         BerlinClock berlinClock = new BerlinClock();
         String expectedMessage = "Unable to convert digital time to Berlin Clock representation";
-        assertEquals(expectedMessage, berlinClock.convert(13,60,0));
+        assertEquals(expectedMessage, berlinClock.convert(hour,min,sec));
     }
+
+
 
 
 
