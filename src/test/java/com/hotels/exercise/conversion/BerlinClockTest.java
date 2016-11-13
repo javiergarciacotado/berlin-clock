eeff4c56118c33e822a2b1df7b9b4abca74319e7
Javier Garcia <jgarciacot@wiley.com>
2016-11-13 13:46:29 +0000

    Refactored test name

--- src/test/java/com/hotels/exercise/conversion/BerlinClockTest.java
+++ src/test/java/com/hotels/exercise/conversion/BerlinClockTest.java
@@ -12,7 +12,7 @@ public class BerlinClockTest {
 
     @Test
     @Parameters({"13, 60, 00", "24, 00, 00", "0, 0, 0"})
-    public void shouldConvert24hTime(int hour, int min, int sec) {
+    public void shouldNotConvertTimeNotIn24hFormat(int hour, int min, int sec) {
         BerlinClock berlinClock = new BerlinClock();
         String expectedMessage = "Unable to convert digital time to Berlin Clock representation";
         assertEquals(expectedMessage, berlinClock.convert(hour,min,sec));
