2da6bf145cb32918a35574eb451abdc83873a9d7
Javier Garcia <jgarciacot@wiley.com>
2016-11-13 13:56:47 +0000

    Refactored second passing test

--- src/test/java/com/hotels/exercise/conversion/BerlinClockTest.java
+++ src/test/java/com/hotels/exercise/conversion/BerlinClockTest.java
@@ -2,6 +2,7 @@ package com.hotels.exercise.conversion;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -10,10 +11,16 @@ import static org.junit.Assert.assertEquals;
 @RunWith(JUnitParamsRunner.class)
 public class BerlinClockTest {
 
+    private BerlinClock berlinClock;
+
+    @Before
+    public void setUp() throws Exception {
+        berlinClock = new BerlinClock();
+    }
+
     @Test
     @Parameters({"13, 60, 00", "24, 00, 00", "0, 0, 0"})
     public void shouldNotConvertTimeNotIn24hFormat(int hour, int min, int sec) {
-        BerlinClock berlinClock = new BerlinClock();
         String expectedMessage = "Unable to convert digital time to Berlin Clock representation";
         assertEquals(expectedMessage, berlinClock.convert(hour,min,sec));
     }
@@ -21,7 +28,6 @@ public class BerlinClockTest {
     @Test
     @Parameters({"Y, 2", "X, 1"})
     public void shouldTurnTopLampYellowEveryTwoSeconds(String expectedResult, int seconds) {
-        BerlinClock berlinClock = new BerlinClock();
         assertEquals(expectedResult, berlinClock.getTopLine(seconds));
     }
 
