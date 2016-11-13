c9ca3d725498bc92fdccb3c27afb6f184e1053e2
Javier Garcia <jgarciacot@wiley.com>
2016-11-13 13:07:58 +0000

    Added first failing test

--- src/main/java/com/hotels/exercise/conversion/BerlinClock.java
+++ src/main/java/com/hotels/exercise/conversion/BerlinClock.java
@@ -1,5 +1,25 @@
 package com.hotels.exercise.conversion;
 
+import java.time.DateTimeException;
+import java.time.LocalTime;
+
 public class BerlinClock {
 
+    public String convert(int hour, int min, int sec) {
+        if (isValidTime(hour, min, sec)) {
+            return "";
+        }
+        return "Unable to convert digital time to Berlin Clock representation";
+    }
+
+    private boolean isValidTime(int hour, int min, int sec)  {
+        try {
+            final LocalTime localTime = LocalTime.of(hour, min, sec);
+            return true;
+        }
+        catch (DateTimeException dateTimeException) {
+            dateTimeException.printStackTrace();
+        }
+        return false;
+    }
 }
--- src/test/java/com/hotels/exercise/conversion/BerlinClockTest.java
+++ src/test/java/com/hotels/exercise/conversion/BerlinClockTest.java
@@ -1,5 +1,19 @@
 package com.hotels.exercise.conversion;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BerlinClockTest {
+
+    @Test
+    public void shouldConvert24hTime() {
+        BerlinClock berlinClock = new BerlinClock();
+        String expectedMessage = "";
+        assertEquals(expectedMessage, berlinClock.convert(13,60,0));
+    }
+
+
+
 
 }
