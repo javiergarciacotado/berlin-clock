106546de88b233839102e5fbae2d170e946598f5
Javier Garcia <jgarciacot@wiley.com>
2016-11-13 14:58:35 +0000

    Added code in main entry point

--- src/main/java/com/hotels/exercise/Main.java
+++ src/main/java/com/hotels/exercise/Main.java
@@ -1,9 +1,20 @@
 package com.hotels.exercise;
 
+import com.hotels.exercise.conversion.BerlinClock;
+
+import java.time.LocalTime;
+
 public class Main {
 
     public static void main(String[] args) {
+        BerlinClock berlinClock = new BerlinClock();
 
+        final LocalTime localTime = LocalTime.now();
+        int hour = localTime.getHour();
+        int minute = localTime.getMinute();
+        int second = localTime.getSecond();
+        System.out.println("CURRENT TIME IN BERLIN CLOCK");
+        System.out.println(berlinClock.convert(hour, minute, second));
     }
 
 }
